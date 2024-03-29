d49d2ec5e928f2090d776d1d22dbc90b71071e98
José Contreras <josemcontrerasp1@gmail.com>
2024-03-29 17:33:44 -0300

    ahora el codigo que recarga los sensores se puede a aplicar a la cantidad de modulos desados partiendo desde el segundo modulo, el siguiente commit será para que parta desde el primero.

--- aparecertabla.js
+++ aparecertabla.js
@@ -5,6 +5,7 @@ function numids(){
     numero++;
     return numero;
 }
+
 function creartabla(){
     const node  =   document.getElementById("modulo 1");
     const clone =   node.cloneNode(true);
@@ -25,18 +26,16 @@ function creartabla(){
             row.children[r].innerHTML="0";
             
         }  
-    }
-    
-
+    }  
 }
-
 
 async function getjson(url){
     const response= await fetch(url);
     return response.json();
 }
-async function recibir(){
 
+async function recibir(){
+    /*modulo 1*/
     const data  = await getjson('http://192.168.1.100/sensores.json');
     let algo = Object.keys(data);
     for(let z of algo){
@@ -44,23 +43,21 @@ async function recibir(){
             document.getElementById("valor 1"+z[7]+a).innerHTML= data[z][a];
         }
     }
-    /*modulo 1*/
-    
     /*los siguientes*/
-    if(!document.getElementById("Modulo 2")){
-        //console.log("no se encuentra el modulo 2");
-    }
-    else{
-        const data2  = await getjson('http://192.168.1.101/sensores.json');
-        algo = Object.keys(data2);
-        for(let z of algo){
-            for(let a = 0; a< data[z].length;a++){
-                document.getElementById("valor 2"+z[7]+a).innerHTML= data2[z][a];
+    for(let p=1; p<3;p++){ // el 3 es el numero de modulos a evaluer
+        if(!document.getElementById("Modulo "+ p)){
+        console.log("no se encuentra el modulo "+ p);
+        }
+        else{
+            const data2  = await getjson('http://192.168.1.10'+p+'/sensores.json');
+            algo = Object.keys(data2);
+            for(let z of algo){
+                for(let a = 0; a < data[z].length;a++){
+                    document.getElementById("valor "+p+z[7]+a).innerHTML= data2[z][a];
+                }
             }
         }
     }
-
-
 }
 
 let ides =[];
