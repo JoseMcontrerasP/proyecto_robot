369fc648fca5c8ded4ea06d4cde4b06d1cc607f4
Yerko Vega <161767947+Yerkovega@users.noreply.github.com>
2024-06-28 14:38:10 -0400

    Add files via upload

--- ServidorModulos/ServidorModulos.ino
+++ ServidorModulos/ServidorModulos.ino
@@ -2,68 +2,120 @@
 #include "ESPAsyncWebServer.h"
 #include "ArduinoJson.h"
 #include "HTTPClient.h"
+#include "ESP32Servo.h"
+#include "Wire.h"
 #include "ScioSense_ENS160.h"
 #include <Adafruit_AHTX0.h>
-#include <Wire.h>
+#include "Adafruit_MPU6050.h"
+#include "Adafruit_Sensor.h"
 
+#define enA 23
+#define in1 19
+#define in2 18
+#define in3 5
+#define in4 17
+#define enB 16
+#define SERVO_PINA 17
+#define SERVO_PINB 16
+#define PIN_ACOPLE 2
+
+int id = 1;/* HAY QUE CAMBIARLO PARA CADA MODULO partiendo desde 1, pq la cabeza no tiene id, 
+              con el id identificamos que modulo es el que debe prenderse.                 */
+const int motorFreq = 1000;
+const int motorResolution = 8;
+const int motorAChannel = 3;
+const int motorBChannel = 4;
+int motorAPWM = 0;
+int motorBPWM = 0;
+bool motorDir = true;
+
+// Set your access point network credentials
 const char* ssid     = "ESP1";
 const char* password = "Passwordsupersegura";
+
 const char* IPhead  = "http://192.168.1.101/RSSI.json";
+const char* IPmov  = "http://192.168.1.101/movstatus";
+const char* IPbrazo  = "http://192.168.1.101/brazostatus";
 const char* IPpost  = "http://192.168.1.101/estadoModulos";
-int answer = 0;
-int comprobador = 0;
 
-int tempC; 
-int humidity; 
-
-Servo sbrazo;
-Adafruit_AHTX0 aht;
-ScioSense_ENS160      ens160(ENS160_I2CADDR_1);
-//...
-
-bool flag = true;
-String nomwifi;
-int id = 2;// HAY QUE CAMBIARLO PARA CADA MODULO partiendo desde 1, pq la cabeza no tiene id, con el id identificamos que modulo es el que debe prenderse.
-// Create AsyncWebServer object on port 80
-int status = 0;
 IPAddress local_IP(192, 168, 1, 101+id);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
+
+int answer = 0;
+int comprobador = 0;
+bool flag = true;
+
+String nomwifi;
+
+int xservoPos = 90;
+Servo xbrazo;
+Servo zbrazo;
+Servo acople;
+
+int humidity; 
+int tempC; 
+Adafruit_MPU6050 mpu;
+Adafruit_AHTX0 aht;
+ScioSense_ENS160      ens160(ENS160_I2CADDR_1);
+int status = 0;
+int rightY;
+int rightX;
+
 AsyncWebServer server(80);
 
-
-JsonDocument readpot() {
+JsonDocument leerSensores() {
   sensors_event_t humidity1, temp;
-  aht.getEvent(&humidity1, &temp);
+  sensors_event_t a, g;
+  JsonDocument  sensores;
+  JsonArray data = sensores["sensor_1"].to<JsonArray>();
+  JsonArray data2= sensores["sensor_2"].to<JsonArray>();
+  JsonArray data3= sensores["sensor_3"].to<JsonArray>();
+  JsonArray data4= sensores["sensor_4"].to<JsonArray>();
+  
+  /*aht.getEvent(&humidity1, &temp);
   tempC = (temp.temperature);
-  humidity = (humidity1.relative_humidity);
+  humidity = (humidity1.relative_humidity);*/
   int MiCS = analogRead(34);
-  Serial.print("Temperatura: "); Serial.print(tempC); Serial.print("°"); Serial.print("\t");
-  Serial.print("Humedad: ");     Serial.print(humidity); Serial.print("% rH "); Serial.print("\t");
+  
+  //AHT2X
+  /*Serial.print("Temperatura: "); Serial.print(tempC); Serial.print("°"); Serial.print("\t");
+  Serial.print("Humedad: ");     Serial.print(humidity); Serial.print("% rH "); Serial.print("\t");*/
+  //MiCS5524
   Serial.print("MiCS5524: ");    Serial.print(MiCS); Serial.println("\t");
-  if (ens160.available()) {
+  //ens160
+  /*if (ens160.available()) {
     ens160.set_envdata(tempC, humidity);
     ens160.measure(true);   ens160.measureRaw(true);
     Serial.print("AQI: ");  Serial.print(ens160.getAQI());Serial.print("\t");
     Serial.print("TVOC: "); Serial.print(ens160.getTVOC());Serial.print("ppb\t");
     Serial.print("eCO2: "); Serial.print(ens160.geteCO2());Serial.println("ppm\t");
-  JsonDocument sensores;
-  JsonArray data = sensores["sensor_1"].to<JsonArray>();
-  JsonArray data2= sensores["sensor_2"].to<JsonArray>();
-  JsonArray data3= sensores["sensor_3"].to<JsonArray>();
-  JsonArray data4= sensores["sensor_4"].to<JsonArray>();
-  data.add(ens160.getAQI());
+  }*/
+  //MPU6050
+  mpu.getEvent(&a, &g, &temp);
+  Serial.print("Aceleración: ");
+  Serial.print("  X: ");Serial.print(a.acceleration.x);
+  Serial.print(", Y: "); Serial.print(a.acceleration.y);
+  Serial.print(", Z: "); Serial.print(a.acceleration.z); 
+  Serial.println(" [m/s^2]");
+  Serial.print("Rotación: ");
+  Serial.print("  X: "); Serial.print(g.gyro.x);
+  Serial.print(", Y: "); Serial.print(g.gyro.y);
+  Serial.print(", Z: "); Serial.print(g.gyro.z);
+  Serial.println(" [rad/s]");
+  Serial.print("Temperatura: "); Serial.print(temp.temperature); Serial.println(" [C]");
+  //Documento Json
+  /*data.add(ens160.getAQI());
   data.add(ens160.getTVOC());
-  data.add(ens160.geteCO2());
+  data.add(ens160.geteCO2());*/
   data2.add(MiCS);
-  data3.add(tempC);
-  data3.add(humidity);
-  data4.add(1);
-  data4.add(2);
-  data4.add(3);
+  data3.add(temp.temperature);
+  //data3.add(humidity);
+  data4.add(g.gyro.x);
+  data4.add(g.gyro.y);
+  data4.add(g.gyro.z);
   return sensores;
-}}
-
+}
 int getRequest(const char* servername){
   HTTPClient http;
   JsonDocument doc;
@@ -97,7 +149,68 @@ int getRequest(const char* servername){
   return resultado;
 
 }
+int getRequestMov(const char* servername){
+  HTTPClient http;
+  http.begin(servername);
+  int resultado;
+  int httpResponseCode=http.GET();
+  while(WiFi.status() == WL_CONNECTED){
+    if(httpResponseCode > 0) {
+      if(httpResponseCode == HTTP_CODE_OK) {
+        resultado=atoi(http.getString().c_str());
+        http.end();
+        return resultado;
+      }
+      else{
+        Serial.print("error mov");
+        resultado = 5;
+        http.end();
+        return resultado;
+      }
+    }
+    else{
+      Serial.println("intentando de nuevo");
+      resultado = getRequestMov(servername);
+      http.end();
+      return resultado;
+    }
+  }  
+  http.end();
+  return resultado;
 
+}
+JsonDocument getRequestBrazo(const char* servername){
+  HTTPClient http;
+  JsonDocument doc;
+  http.useHTTP10(true);
+  http.begin(servername);
+  int httpResponseCode=http.GET();
+  while(WiFi.status() == WL_CONNECTED){
+    if(httpResponseCode > 0) {
+      if(httpResponseCode == HTTP_CODE_OK) {
+        deserializeJson(doc, http.getStream());
+        http.end();
+        return doc;
+      }
+      else{
+        Serial.print("error brazo");
+        http.end();
+        JsonDocument error;
+        error["error"]=1;
+        return error;
+      }
+    }
+    else{
+      Serial.println("intentando de nuevo");
+      http.end();
+      doc = getRequestBrazo(servername);
+      return doc;
+    }
+  }  
+  http.end();
+  return doc;
+
+}
 void postRequest(const char* servername, int identificador, int status){
   HTTPClient http;
   String estado = "?modulo="  + String(identificador)  + "&status=" + String(status);
@@ -123,17 +236,48 @@ String agregar(String nom){       //codigo que genera el siguiente SSID al cual 
   }
   return nombre;
 }
+void movBrazo(){
+  if(rightY < -100){
+    xservoPos = 90;
+    xbrazo.write(xservoPos);
+    delay(10);
+  }else{
+    if (rightX < -10 && xservoPos < 180) {
+      xservoPos++;
+      xbrazo.write(xservoPos);
+      delay(10);
+      }
+    if (rightX > 10 && xservoPos > 0) {
+      xservoPos--;
+      xbrazo.write(xservoPos);
+      delay(10);
+    }
+  }
+}
+
+void moveMotors(int mtrAspeed, int mtrBspeed, bool mtrdir) {
+  if (!mtrdir) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+  } 
+  else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+  }
+  ledcWrite(motorAChannel, mtrAspeed);
+  ledcWrite(motorBChannel, mtrBspeed);
+}
+
 
 void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("STA Failed to configure");
-  }
-  
-  //Configuracion de sensores...
-  ens160.begin();
+  //Puerto Serial
+  Serial.begin(115200); 
+  //Sensores
+  /*ens160.begin();
   Serial.println(ens160.available() ? "done." : "failed!");
   if (ens160.available()) {
     Serial.print("\tRev: "); Serial.print(ens160.getMajorRev());
@@ -141,20 +285,107 @@ void setup(){
     Serial.print("."); Serial.println(ens160.getBuild());
     Serial.println(ens160.setMode(ENS160_OPMODE_STD) ? "done." : "failed!");
   }
-  if (! aht.begin()) {
+  if (!aht.begin()) {
     Serial.println("Could not find AHT? Check wiring");
     while (1) delay(10);
+    Serial.println("AHT20 no detectado");
+  }*/
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
   }
-  Serial.println("AHT20 no detectado");
-  //...
+  Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+  }
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+  }
 
-  pinMode(32,OUTPUT);//servo union
-  pinMode(33, OUTPUT);//switch comunicacion(router)
+  //Servos eje X,Z y acople 
+  xbrazo.attach(SERVO_PINA);
+  xbrazo.write(xservoPos);
+  zbrazo.attach(SERVO_PINB);
+  zbrazo.write(xservoPos);
+  acople.attach(PIN_ACOPLE);
+  acople.write(90);
+  pinMode(32,OUTPUT); //LED indicador de acople 
   digitalWrite(32,HIGH);
+  //La señal para el switch de comunicación
+  pinMode(33, OUTPUT);//switch comunicacion(router) 
   digitalWrite(33,LOW);
-
-  WiFi.begin(ssid,password);
+  //Motores DC
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  ledcSetup(motorAChannel, motorFreq, motorResolution);
+  ledcSetup(motorBChannel, motorFreq, motorResolution);
+  ledcAttachPin(enA, motorAChannel);
+  ledcAttachPin(enB, motorBChannel);
+  //configuración del WIFi
+  WiFi.mode(WIFI_STA);
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("STA Failed to configure");
+  }
   WiFi.setSleep(false);
+  //iniciación de wifi
+  WiFi.begin(ssid,password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -166,15 +397,16 @@ void setup(){
   Serial.println(nomwifi);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+  
 }
 
 void loop(){  
   if (answer == id){
     while(flag){
-      Serial.println("Desconectando modulo de la cola");
+      Serial.println("desconectando modulo de la cola");
       digitalWrite(32,  LOW);// esto debería hace que el servo o stepper que haga de gancho se desenganche.
-      delay(3000);
+      acople.write(120);
+      delay(3000);//tiempo para que se desenganche.
       
       Serial.println("Prendiendo nuevo modulo de conexión");
       digitalWrite(33,  HIGH);//este pin lo que haria seria activar un relee o algo que haga switch que prenda el router o esp32 que hace de nodo de conexión;
@@ -197,7 +429,7 @@ void loop(){
       Serial.println("servidor configurandose");
       server.on("/sensores.json", HTTP_GET, [](AsyncWebServerRequest *request){
         String response; 
-        serializeJsonPretty(readpot(), response);
+        serializeJsonPretty(leerSensores(), response);
         request->send(200, "application/json", response);
       });
       server.on("/ping",HTTP_GET, [](AsyncWebServerRequest *request){
@@ -236,6 +468,21 @@ void loop(){
     }
     //aqui colocar el codigo del movimiento.
     comprobador = answer;
+    int mov = getRequestMov(IPmov);
+    if( mov !=  -1){
+      motorAPWM =  255;
+      motorBPWM =  255;
+      moveMotors(motorAPWM,motorBPWM,mov);
+    }
+    else{
+      moveMotors(0,0,mov);
+    }
+    JsonDocument algo =  getRequestBrazo(IPbrazo);
+    if(algo["brazostatus"]  ==  id){
+      rightY=algo["Y"];
+      rightX=algo["X"];
+      movBrazo();
+    }
     delay(200);  
   }
 }
